79eac03acf64c498de8051cb1bc865bc0bff856d
Hryts <hrytsay@ucu.edu.ua>
2020-03-07 20:01:15 +0200

    bbla bla

--- python_script/script.py
+++ python_script/script.py
@@ -1,4 +1,20 @@
 import os
-cmd = './integral ../configuration_file.txt'
-so = os.popen(cmd).read()
-print(so)
+import sys
+
+
+cmd = './integral ./configuration_file.txt'
+
+min_time = float('inf')
+reps = int(sys.argv[1])
+results = []
+for _ in range(reps):
+    print(_)
+    so = os.popen(cmd).read()
+    min_time = min(min_time, (float(so.split()[-1][:-1])))
+    results.append(float(so.split()[2]))
+
+results.sort()
+
+
+
+print(min_time)
